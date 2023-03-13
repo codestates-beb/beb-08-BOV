df659cc48e086e531b2851e1302a76686ff03544
soma <macaronieyes@gmail.com>
2023-03-13 09:15:41 +0900

    페이지 이동시 props 받아올수 있도록 수정

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
+import { useLocation } from 'react-router-dom';
 
 import '../style/DetailPage.css';
 import {delay, changeParam} from '../utils/common';
 import DetailPageInfo1 from './DetailPageInfo1';
 import DetailPageInfo2 from './DetailPageInfo2';
-//import NFTDetailPage from './NFTDetailPage';
+import NFTDetailPage from './NFTDetailPage';
 import Assets from './Assets';
-//import {getContractMetadata, getNftsForContract, getNftSales} from '../apis/alchemy';
+//import {getContractMetadata, getNftsForContract, getNftSales,} from '../apis/alchemy';
 import {getCollection, getAssets} from '../apis/opensea';
 
-export default function DetailPage({collectionSlug, collectionAddr}) {
+export default function DetailPage(props) {
   const [collectionData, setCollectionData] = useState();
-  const [assetsData, setAssetsData] = useState();
+  const [assetsData, setAssetsData] = useState(); 
 
+  //props 설정
+  const location = useLocation();
+  let {collectionSlug, collectionAddr} = props;  
+  if(!collectionSlug || !collectionAddr) {
+    if(location.state) {
+      ({collectionSlug, collectionAddr} = location.state);
+    }
+  }
+console.log(collectionSlug,collectionAddr)
   useEffect (() => {
     //0x6447F7d21f19af6c11824B06E3a6618542ceDF33 //주소창에 뜨는 주소
     //0xb4d06d46A8285F4EC79Fd294F78a881799d8cEd9 //이더스캔에 검색되는 주소
     // getNftsForContract('0xb4d06d46A8285F4EC79Fd294F78a881799d8cEd9').then((res) => {
-    //   setnftsData(res);
-    //   //console.log("NFT", res);
+    //   //setnftsData(res);
+    //   console.log("NFT", res);
     // }) 
+
     (async () => {
       //collection 정보 가져오기
       getCollection(collectionSlug).then((res) => {
@@ -28,29 +39,31 @@ export default function DetailPage({collectionSlug, collectionAddr}) {
         console.log("colll", res.data.collection);       
       })
       await delay(1100);
-       //collection stats 정보 가져오기
-      getAssets(collectionAddr).then((res) => {
+      //NFT 정보 가져오기
+      getAssets(collectionAddr, collectionSlug).then((res) => {
         setAssetsData(res.data.assets);
         console.log("assets", res.data.assets);
-      }) 
+      })      
     })();       
-  }, [])
- 
-  // if(!assetsData) return <></>
-  // return (
-  //   <>
-  //     <NFTDetailPage assetData={assetsData[1]}/>
-  //   </>
-  // )
+  }, [])  
 
   if(!collectionData 
     ) return <></>
+
+  // if(!assetsData) return <></>
+  // return (
+  //   <>
+  //     <NFTDetailPage assetData={assetsData[1]} collTraits={collectionData.traits}/>
+  //   </>
+  // )  
 
   return(
   <div id='detailPage'>
     <section id="top">
       <div className='banner'>
-        <img src={changeParam(collectionData.banner_image_url, 'w', 3840)} alt="banner"/>
+        {collectionData.banner_image_url ? 
+        <img src={changeParam(collectionData.banner_image_url, 'w', 3840)} alt="banner"/> :
+        <></>}
       </div>
     </section>
     <section id="middle">
@@ -68,13 +81,13 @@ export default function DetailPage({collectionSlug, collectionAddr}) {
         <DetailPageInfo2 collectionData={collectionData}/>
       </div>
     </section>
-    <section id="bottom">
+    <section id="bottom">      
       <div className='tab'>
         <div className='title'>
           <div className='title-items title-focus'>Items</div>
         </div>
         <div className='items'>
-          {assetsData ? <Assets assetsData={assetsData}/> : <></> }          
+          {assetsData ? <Assets assetsData={assetsData} collTraits={collectionData.traits}/> : <></> }          
         </div>
       </div>      
     </section>
