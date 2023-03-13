86d03a4e1ddb4427d526d290788802065085e6fe
soma <macaronieyes@gmail.com>
2023-03-13 09:39:49 +0900

    main 페이지 복구

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -6,7 +6,7 @@ export default function MainPage() {
   return (
   <div>
     메인 페이지
-    <DetailPage collectionSlug={"pvp-blackhole"} 
-    collectionAddr={"0xf4910c763ed4e47a585e2d34baa9a4b611ae448c"}/>
+    {/* <DetailPage collectionSlug={"pvp-blackhole"} 
+    collectionAddr={"0xf4910c763ed4e47a585e2d34baa9a4b611ae448c"}/> */}
   </div>);
 }
